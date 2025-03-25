e019a1129b875332178a243c14f1689b59c3170c
JoaquinTena <joaquintch2005@gmail.com>
2025-03-25 08:47:08 +0100

    COMMIT CLASE COCHE

---
+++ src/DGTMODEL/COCHE.java
@@ -0,0 +1,19 @@
+package DGTMODEL;
+
+public class COCHE {
+
+    public class Coche extends Vehiculo {
+        private int numAsientos;
+        private boolean tieneAirbags;
+
+        public Coche(String matricula, String marca, int km, int anoFabricacion, PROPIETARIO.Propietario propietario, int numAsientos, boolean tieneAirbags) {
+            super(matricula, marca, km, anoFabricacion, propietario);
+            this.numAsientos = numAsientos;
+            this.tieneAirbags = tieneAirbags;
+        }
+
+        public boolean tieneSillitaBebe() {
+            return numAsientos > 4;
+        }
+    }
+}
