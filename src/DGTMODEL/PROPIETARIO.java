a0f1e89023bb742028cdf2313758cd5606037a9c
JoaquinTena <joaquintch2005@gmail.com>
2025-03-25 08:40:38 +0100

    COMMIT CLASE PROPIETARIO

---
+++ src/DGTMODEL/PROPIETARIO.java
@@ -0,0 +1,28 @@
+package DGTMODEL;
+
+public class PROPIETARIO {
+
+    public class Propietario {
+        private String dni;
+        private String nombre;
+        private String apellidos;
+        private int puntos;
+
+        public Propietario(String dni, String nombre, String apellidos, int puntos) {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.puntos = puntos;
+        }
+
+        public String getDni() { return dni; }
+        public String getApellidos() { return apellidos; }
+
+        @Override
+        public String toString() {
+            return "DNI: " + dni + ", Nombre: " + nombre + " " + apellidos + ", Puntos: " + puntos;
+        }
+    }
+
+
+}
